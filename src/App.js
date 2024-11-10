d88e30029b4beb33720ddf4eee8bea7d7a6eaac2
Lulwa Alkanderi <l.alkindri@gmail.com>
2024-11-10 20:43:20 +0300

    task 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css"
+import img from "./books.webp";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>The Book Shop</h1>
+      <p>Where you forget the world</p>
+      <img className="Img" src={img}></img>
     </div>
   );
 }
