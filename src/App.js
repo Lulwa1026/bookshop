e38eb8836b4e838234e1c0636485f55a6427a4d7
Lulwa Alkanderi <l.alkindri@gmail.com>
2024-11-11 19:02:15 +0300

    task3

--- src/App.js
+++ src/App.js
@@ -6,33 +6,37 @@ function App() {
   const products = [
     {
       name: "islamic",
-      price: "18 KD",
+      price: "17 KD",
+      img: b1,
     },
     {
       name: "learn Arabic",
       price: "7 KD",
+      img: b2,
     }
   ];
+  const productsList = products.map((product, index)=> {
+    return(
+      <div key={index}>
+        <div>
+      <div>
+        <img className="Imgs" src={product.img}></img>
+        <h3>{product.name}</h3>
+        <p>{product.price}</p>
+      </div>
+      </div>
+      </div>
+    )}
+  )
   return (
     <div className="App">
       <h1>The Book Shop</h1>
       <p>Where you forget the world</p>
       <img className="Img" src={img}></img>
-
       <div className="Products">
-      <div>
-        <img className="Imgs" src={b1}></img>
-        <h3>{products[0].name}</h3>
-        <p>{products[0].price}</p>
+      {productsList}
       </div>
-
-      <div>
-        <img className="Imgs" src={b2}></img>
-        <h3>{products[1].name}</h3>
-        <p>{products[1].price}</p>
-      </div>
-      </div>
-
+      
     </div>
   );
 }
